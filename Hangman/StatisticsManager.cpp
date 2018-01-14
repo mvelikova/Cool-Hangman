9b048c309ae9e3cba203dfa95855b7b829212c25
Maria Velikova <maria.y.velikova@gmail.com>
2018-01-14 14:41:52 +0200

    Added statistics function for the 50/50 algorithm

--- Hangman/StatisticsManager.cpp
+++ Hangman/StatisticsManager.cpp
@@ -11,6 +11,37 @@ StatisticsManager::~StatisticsManager()
 {
 }
 
+char StatisticsManager::Get50percentUsedLetter(const std::vector<std::string>& words)
+{
+	const int lowercase_offset = 97;
+	const int english_alphabet_size = 26;
+	int characterOccurances[english_alphabet_size] = { 0 };
+
+	for (auto word : words)
+	{
+		for (auto character : word)
+		{
+			int idx = character - lowercase_offset;
+			characterOccurances[idx]++;
+		}
+	}
+	int sum = 0;
+	for (int i = 0; i < english_alphabet_size; ++i)
+	{
+		sum += characterOccurances[i];
+	}
+	double percentOfSum = 100.0 / sum;
+	int smallestDiffIndex = 0;
+	for (int i = 1; i < english_alphabet_size; ++i)
+	{
+		if (50 - (characterOccurances[smallestDiffIndex] * percentOfSum)>50 - (characterOccurances[i] * percentOfSum))
+		{
+			smallestDiffIndex = i;
+		}
+	}
+	return smallestDiffIndex + lowercase_offset;
+}
+
 char StatisticsManager::GetMostCommonLetter(const std::vector<std::string>& words)
 {
 	const int lowercase_offset = 97;
--- Hangman/StatisticsManager.h
+++ Hangman/StatisticsManager.h
@@ -6,6 +6,7 @@ class StatisticsManager
 public:
 	StatisticsManager();
 	~StatisticsManager();
-
+	char Get50percentUsedLetter(const std::vector<std::string>& words);
 	char GetMostCommonLetter(const std::vector<std::string>& words);
+
 };
